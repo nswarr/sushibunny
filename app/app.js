81a961de186e8337ab1ed9f57be545b8d1c0f55a
nswarr <nswarr@glgroup.com>
2015-07-24 19:42:59 -0400

    Just ran through the tutrial, have a walking stickman.  Yeehaw\!

--- app/app.js
+++ app/app.js
@@ -1,16 +1,88 @@
-var Phaser = require('phaser');
+//var Phaser = require('phaser');
 
 window.onload = function() {
 
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+  var game = new Phaser.Game(640, 480, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+  var platforms;
+  var player;
+  var cursors;
 
   function preload () {
-    game.load.image('logo', 'assets/message-sushi.png');
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.spritesheet('stickman', 'assets/stickman.png', 48, 48);
+    cursors = game.input.keyboard.createCursorKeys();
   }
 
   function create () {
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.add.sprite(0, 0, 'sky');
+    game.add.sprite(0, 0, 'stickman');
+
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
+    platforms.enableBody = true;
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(2, 2);
+
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+
+
+       // The player and its settings
+    player = game.add.sprite(32, game.world.height - 150, 'stickman');
+
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(player);
+
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [1, 2], 10, true);
+    player.animations.add('right', [2, 1], 10, true);
+  }
+
+  function update() {
+    game.physics.arcade.collide(player, platforms);
+
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+
+        player.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 0;
+    }
+
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
   }
 
 };
